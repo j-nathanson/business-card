e40cbaf5533dc6e2fab8eebefabc78d7239b5d41
jacobnathanson <jacobnath93@gmail.com>
2021-12-29 00:27:19 -0500

    very basic component and lorem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Headshot from './components/Headshot';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey bob</h1>
+    <div className="container-fluid shell pt-4 pb-4">
+      <div className="container">
+        <div className="col content">
+          <div className="row justify-content-center"> <Headshot /></div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempore est eligendi iusto, architecto hic doloremque omnis aliquid et illum dolores voluptas! Officia omnis neque earum a recusandae veritatis. Vel, deserunt autem, totam officia nobis corporis quod quos dicta ea minima non! Modi officia, est excepturi dolorum unde animi quod quia autem enim exercitationem, non atque dicta natus corrupti accusamus sunt distinctio, impedit nostrum aliquid magnam aut? Deserunt aut veniam debitis eaque nam! Optio odio aut quos expedita? Exercitationem incidunt, et velit ipsa sed cum quibusdam. Delectus laborum minima ipsam fuga deserunt, quaerat quasi, esse nam a tempore aperiam eligendi.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempore est eligendi iusto, architecto hic doloremque omnis aliquid et illum dolores voluptas! Officia omnis neque earum a recusandae veritatis. Vel, deserunt autem, totam officia nobis corporis quod quos dicta ea minima non! Modi officia, est excepturi dolorum unde animi quod quia autem enim exercitationem, non atque dicta natus corrupti accusamus sunt distinctio, impedit nostrum aliquid magnam aut? Deserunt aut veniam debitis eaque nam! Optio odio aut quos expedita? Exercitationem incidunt, et velit ipsa sed cum quibusdam. Delectus laborum minima ipsam fuga deserunt, quaerat quasi, esse nam a tempore aperiam eligendi.</p>
+        </div>
+      </div>
+
+
     </div>
   );
 }
---
+++ src/components/Headshot.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Headshot() {
+    return (
+        <div>
+            <div className="headshot--box"></div>
+        </div>
+
+    );
+}
+
+export default Headshot;
