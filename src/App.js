62e611d64c251fe47ef610a6118832009561bf9f
jacobnathanson <jacobnath93@gmail.com>
2021-12-29 15:24:36 -0500

    header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './style.css';
 import Headshot from './components/Headshot';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="container-fluid shell pt-4 pb-4">
       <div className="container content">
-
-        <div className="row justify-content-center"> <Headshot /></div>
-
-        <h1>bob bobson</h1>
-        <h2>bob bobson</h2>
-        <h3>bob bobson</h3>
-
+        <div className="row justify-content-center">
+          <Headshot />
+        </div>
+        <div className="row justify-content-center text-center">
+          <Header />
+        </div>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempore est eligendi iusto, architecto hic doloremque omnis aliquid et illum dolores voluptas! Officia omnis neque earum a recusandae veritatis. Vel, deserunt autem, totam officia nobis corporis quod quos dicta ea minima non! Modi officia, est excepturi dolorum unde animi quod quia autem enim exercitationem, non atque dicta natus corrupti accusamus sunt distinctio, impedit nostrum aliquid magnam aut? Deserunt aut veniam debitis eaque nam! Optio odio aut quos expedita? Exercitationem incidunt, et velit ipsa sed cum quibusdam. Delectus laborum minima ipsam fuga deserunt, quaerat quasi, esse nam a tempore aperiam eligendi.</p>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function Header() {
     return (
-        <div>
-            
+        <div className="">
+            <h1>bob bobson</h1>
+            <h2>bob bobson</h2>
+            <h3>bob bobson</h3>
+
         </div>
 
     );
