5ed71f0edbfebb955a2f15bbd4b2f2ec5ac2a113
jacobnathanson <jacobnath93@gmail.com>
2021-12-29 15:52:32 -0500

    fix width and top radius

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './style.css';
+import { Col } from 'reactstrap';
 import Headshot from './components/Headshot';
 import Header from './components/Header';
 
@@ -11,6 +12,14 @@ function App() {
         </div>
         <div className="row justify-content-center text-center">
           <Header />
+        </div>
+        <div className="row ">
+          <div className="col-8 d-flex justify-content-around mx-auto">
+            <a href="" className='btn btn-dark'>hey click</a>
+            <a href="" className='btn btn-info'>hey click</a>
+          </div>
+
+
         </div>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tempore est eligendi iusto, architecto hic doloremque omnis aliquid et illum dolores voluptas! Officia omnis neque earum a recusandae veritatis. Vel, deserunt autem, totam officia nobis corporis quod quos dicta ea minima non! Modi officia, est excepturi dolorum unde animi quod quia autem enim exercitationem, non atque dicta natus corrupti accusamus sunt distinctio, impedit nostrum aliquid magnam aut? Deserunt aut veniam debitis eaque nam! Optio odio aut quos expedita? Exercitationem incidunt, et velit ipsa sed cum quibusdam. Delectus laborum minima ipsam fuga deserunt, quaerat quasi, esse nam a tempore aperiam eligendi.</p>
